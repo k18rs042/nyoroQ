eeb5fcd8f5c048af53c803ac58c894ec417f2cb5
Yoshiki KUMAGAI <k18rs042@st.kyusan-u.ac.jp>
2020-06-30 17:24:46 +0900

    変数の追加

--- ColorDetection.h
+++ ColorDetection.h
@@ -4,6 +4,8 @@
 #define BLACK           2       //黒
 #define RED             3       //赤
 #define GLAY            4      //灰
+#define NEARWHITE       5      //
+#define NEARBLACK       6
 
 //色検出
 int DetectionColor(int color_sensor);
